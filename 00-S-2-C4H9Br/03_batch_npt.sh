9ab4535bc4826880059629406c13126bd5f9e545
rstric2s <robin.strickstrock@smail.emt.h-brs.de>
2025-06-11 11:15:31 +0200

    check structure

--- 00-S-2-C4H9Br/03_batch_npt.sh
+++ 00-S-2-C4H9Br/03_batch_npt.sh
@@ -2,8 +2,8 @@
 #SBATCH --partition=hpc,hpc1,hpc3
 #SBATCH --nodes=1
 #SBATCH --mem 1G
-#SBATCH --time=24:00:00
-#SBATCH --job-name=1Cnpt
+#SBATCH --time=02:00:00
+#SBATCH --job-name=2Cnpt
 
 gmx grompp -f 03_npt.mdp -c 12_nvt.gro -t 12_nvt.trr -p topol.top -o 13_npt.tpr
 gmx mdrun -deffnm 13_npt
